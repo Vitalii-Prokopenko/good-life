2ecd729a10e849a12c97ee64bbccd3340d7b08fd
Vitalii-Prokopenko <doc8259@gmail.com>
2023-01-07 08:49:33 +0200

    faq img and styles added

--- js/swiper.js
+++ js/swiper.js
@@ -1,25 +1,34 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+  wrapperClass: 'hero__swiper-wrapper',
+  slideClass: 'hero__swiper-slide',
   direction: 'horizontal',
   loop: true,
   centeredSlides: true,
-  effect: 'flipe',
+  effect: 'slide',
   grabCursor: true,
-  speed: 300,
+  speed: 1000,
+
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-      },
+    el: '.hero__swiper-pagination',
+    bulletClass: 'hero__swiper-pagination-bullet',
+    bulletActiveClass: 'hero__swiper-pagination-bullet-active',
+  },
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
